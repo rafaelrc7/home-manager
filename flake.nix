658bc329363dcd7be3cc4bc21fe04104fcea04a9
rafaelrc7 <rafaelrc7@gmail.com>
2022-04-16 02:26:58 -0300

    Added terminal (kitty) config

--- flake.nix
+++ flake.nix
@@ -47,6 +47,23 @@
             };
           };
 
+          programs.kitty = {
+            enable = true;
+            font = {
+              package = pkgs.nerdfonts;
+              name = "FiraCode Nerd Font Mono";
+              size = 12;
+            };
+            settings = {
+              bold_font = "auto";
+              italic_font = "auto";
+              bold_italic_font = "auto";
+              enable_audio_bell = false;
+              scrollback_pager_history_size = 2048;
+              mouse_map = "left click ungrabbed no-op";
+            };
+          };
+
           systemd.user.startServices = "sd-switch";
         };
 
