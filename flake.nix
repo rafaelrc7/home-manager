d0f1ea0d3502c5c64f5415e7e4a384fdbe904965
rafaelrc7 <rafaelrc7@gmail.com>
2022-04-27 15:41:32 -0300

    Add nvim lsp config

--- flake.nix
+++ flake.nix
@@ -84,23 +84,180 @@
             withPython3 = true;
             withNodeJs = true;
 
-            extraConfig =''
-              lua require('init')
+            extraConfig = with pkgs; ''
+              lua << EOF
+              require('init')
+
+              -- LSP
+              local utils = require "utils"
+              local nvim_lsp = require "lspconfig"
+              local pid = vim.fn.getpid()
+              local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+              local function on_attach()
+              end
+
+              vim.g.completion_matching_strategy_list = {"exact", "substring", "fuzzy"};
+
+              -- CLANGD
+              nvim_lsp.clangd.setup {
+                cmd = { "${clang}/bin/clangd" },
+                on_attach = on_attach,
+                filetypes = { "c", "cpp", "h", "hpp", "objc", "objcpp", "cuda" },
+                root_dir = function() return vim.loop.cwd() end,
+                capabilities = capabilities,
+              };
+
+              -- Rust
+              nvim_lsp.rust_analyzer.setup{
+                cmd = { "${rust-analyzer}/bin/rust-analyzer" },
+                on_attach=on_attach,
+                capabilities = capabilities,
+              };
+
+              -- Elixir
+              nvim_lsp.elixirls.setup{
+                cmd = { "${elixir_ls}/bin/elixir-ls" },
+                on_attach = on_attach,
+                capabilities = capabilities,
+              };
+
+              -- Haskell (hls)
+              nvim_lsp.hls.setup{
+                on_attach = on_attach,
+                capabilities = capabilities,
+              };
+
+              -- html
+              capabilities.textDocument.completion.completionItem.snippetSupport = true;
+
+              nvim_lsp.html.setup {
+                cmd = {"${nodePackages.vscode-langservers-extracted}/bin/vscode-html-language-server", "--stdio"},
+                on_attach = on_attach,
+                capabilities = capabilities,
+              };
+
+              -- CSS
+              nvim_lsp.cssls.setup {
+                cmd = {"${nodePackages.vscode-langservers-extracted}/bin/vscode-css-language-server", "--stdio"},
+                on_attach = on_attach,
+                capabilities = capabilities,
+              }
+
+              -- TS
+              nvim_lsp.tsserver.setup{
+                cmd = {"${nodePackages.typescript-language-server}/bin/tsserver", "--stdio"},
+                on_attach=on_attach,
+                capabilities = capabilities,
+              };
+
+              -- Json
+              nvim_lsp.jsonls.setup {
+                cmd = {"${nodePackages.vscode-langservers-extracted}/bin/vscode-json-language-server", "--stdio"},
+                on_attach = on_attach,
+                commands = {
+                  Format = {
+                    function()
+                      vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0});
+                    end
+                  },
+                },
+                capabilities = capabilities,
+              };
+
+              -- C# (Omnisharp)
+              nvim_lsp.omnisharp.setup{
+                cmd = { "${omnisharp-roslyn}/bin/omnisharp", "--languageserver" , "--hostPID", tostring(pid) },
+                on_attach = on_attach,
+                root_dir = nvim_lsp.util.root_pattern("*.csproj","*.sln"),
+                capabilities = capabilities,
+              };
+
+              -- Python (pyright)
+              nvim_lsp.pyright.setup{
+                cmd = {"${pyright}/bin/pyright-langserver", "--stdio"},
+                on_attach = on_attach,
+                settings = {
+                  python = {
+                    analysis = {
+                      extraPaths = {".", "src"},
+                    },
+                  },
+                },
+                capabilities = capabilities,
+              };
+
+              -- Lua (sumneko)
+              local runtime_path = vim.split(package.path, ';')
+              table.insert(runtime_path, "lua/?.lua")
+              table.insert(runtime_path, "lua/?/init.lua")
+
+              nvim_lsp.sumneko_lua.setup {
+                cmd = {"${sumneko-lua-language-server}/bin/lua-language-server"},
+                on_attach = on_attach,
+                settings = {
+                  Lua = {
+                    runtime = {
+                      -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                      version = 'LuaJIT',
+                      -- Setup your lua path
+                      path = runtime_path,
+                    },
+                    diagnostics = {
+                      -- Get the language server to recognize the `vim` global
+                      globals = {'vim'},
+                    },
+                    workspace = {
+                      -- Make the server aware of Neovim runtime files
+                      library = {
+                        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                      },
+                    },
+                    -- Do not send telemetry data containing a randomized but unique identifier
+                    telemetry = {
+                      enable = false,
+                    },
+                  },
+                },
+                capabilities = capabilities,
+              };
+
+              -- LaTeX (texlab)
+              nvim_lsp.texlab.setup{
+                cmd = { "${texlab}/bin/texlab" },
+                on_attach = on_attach,
+                capabilities = capabilities,
+              };
+
+              -- Vim
+              nvim_lsp.vimls.setup{
+                cmd = { "${nodePackages.vim-language-server}/bin/vim-language-server" },
+                on_attach = on_attach,
+                capabilities = capabilities,
+              };
+
+              -- Nix (rnix-lsp)
+              nvim_lsp.rnix.setup{
+                cmd = { "${rnix-lsp}/bin/rnix-lsp" },
+                on_attach = on_attach,
+                capabilities = capabilities,
+              };
+
+              utils.nvim_create_augroups(
+                {
+                  lspconfig = {
+                    {"CursorHold", "*", "lua vim.diagnostic.open_float()"},
+
+                    -- Java (jdtls)
+                    {"FileType", "java", "lua require('jdtls').start_or_attach({cmd = {'jdtls.sh'}})"}
+                  },
+                }
+              );
+              EOF
             '';
 
-            extraPackages = with pkgs; [
-              clang
-              rust-analyzer
-              elixir_ls
-              nodePackages.vscode-langservers-extracted
-              nodePackages.typescript-language-server
-              nodePackages.pyright
-              sumneko-lua-language-server
-              texlab
-              rnix-lsp
-            ];
-
-            plugins = with pkgs; with vimPlugins; [
+            plugins = with pkgs.vimPlugins; [
               nvim-treesitter
               #(nvim-treesitter.withPlugins (plugins: tree-sitter.allGrammars))
               nvim-lspconfig
